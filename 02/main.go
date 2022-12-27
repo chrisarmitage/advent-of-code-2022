3c046879ac1884f59a1ba27b9b298805d01102a6
Chris Armitage <dev@chrisarmitage.com>
2022-12-27 20:22:17 +0000

    Day 2, part 2

--- 02/main.go
+++ 02/main.go
@@ -17,20 +17,28 @@ func main() {
 
 	fileScanner.Split(bufio.ScanLines)
 
-	score := 0
+	scoreP1 := 0
+	scoreP2 := 0
 
 	var line string
 	var movePoints int
 	var resultPoints int
+	var p2Points int
 
 	// A X = R
 	// B Y = P
 	// C Z = S
+
+	// Required result
+	// X = Lose
+	// Y = Draw
+	// Z = Win
     for fileScanner.Scan() {
         line = fileScanner.Text()
 
 		s := strings.Split(line, " ")
     	theirMove, myMove := s[0], s[1]
+		targetResult := s[1]
 
 		if (
 			(theirMove == "A" && myMove == "X") ||
@@ -56,12 +64,50 @@ func main() {
 			movePoints = 3
 		}
 
-		score += (movePoints + resultPoints)
+		// Strategy 2
+		// A X = R
+		// B Y = P
+		// C Z = S
+
+		// Required result
+		// X = Lose
+		// Y = Draw
+		// Z = Win
+		if (theirMove == "A") {
+			if (targetResult == "X") {
+				p2Points = 3 + 0
+			} else if (targetResult == "Y") {
+				p2Points = 1 + 3
+			} else {
+				p2Points = 2 + 6
+			}
+		} else if (theirMove == "B") {
+			if (targetResult == "X") {
+				p2Points = 1 + 0
+			} else if (targetResult == "Y") {
+				p2Points = 2 + 3
+			} else {
+				p2Points = 3 + 6
+			}
+		} else {
+			if (targetResult == "X") {
+				p2Points = 2 + 0
+			} else if (targetResult == "Y") {
+				p2Points = 3 + 3
+			} else {
+				p2Points = 1 + 6
+			}
+		}
+
+
+		scoreP1 += (movePoints + resultPoints)
+		scoreP2 += p2Points
 	}
 	
     readFile.Close()
 
-	fmt.Printf("Final score: %d\n", score)
+	fmt.Printf("Final score (strategy one): %d\n", scoreP1)
+    fmt.Printf("Final score (strategy two): %d\n", scoreP2)
     
 
 }
