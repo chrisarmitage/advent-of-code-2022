22943b0ce11944a405c1056939c399d30ee9682c
Chris Armitage <dev@chrisarmitage.com>
2022-12-27 22:22:29 +0000

    Day 3, part 2

--- 03/main.go
+++ 03/main.go
@@ -6,12 +6,18 @@ import (
 	"os"
 )
 
-const SAMPLE_TARGET = 157
+const SAMPLE_TARGET_P1 = 157
+const SAMPLE_TARGET_P2 = 70
 
 func main() {
 	scanner := makeScanner("input.txt")
 
 	total := 0 
+
+	resultP2 := calculatePart2(scanner)
+
+	
+	scanner = makeScanner("sample.txt")
 
 	for scanner.Scan() {
         line := scanner.Text()
@@ -55,7 +61,58 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Total priorities: %d", total)
+	fmt.Printf("Part 1: Total priorities = %d\n", total)
+
+	fmt.Printf("Part 2: Total priorities = %d\n", resultP2)
+}
+
+func calculatePart2(scanner *bufio.Scanner) int {
+	var jointBitmask uint64
+	
+
+	
+	result := 0
+
+	for scanner.Scan() {
+		for n := 0; n <= 63; n++ {
+			jointBitmask |= 1 << n
+		}
+		fmt.Printf("Start %64b\n", jointBitmask)
+
+
+		// Get three lines
+		for n := 1; n <= 3; n++ {
+			var lineBitmask uint64
+
+			if n != 1 {
+				scanner.Scan()
+			}
+			line := scanner.Text()
+
+			for _, item := range line {
+				// Convert each item into value
+				priority := getPriority(item)
+				
+				// Set bitmask
+				lineBitmask |= uint64(1 << priority)
+			}
+
+			jointBitmask &= lineBitmask
+			
+			fmt.Printf(" Pass %64b\n", lineBitmask)
+			fmt.Printf(" Res  %64b\n", jointBitmask)
+		}
+
+
+		for key := 1; key <= 52; key++ {
+			if jointBitmask & uint64(1 << key) != 0 {
+				result += key
+			}
+		}
+	}
+
+
+	return result
 }
 
 func getPriority(item rune) int {
