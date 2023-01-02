de8763f2a5d8e43fa2e468916479c7076cfe8312
Chris Armitage <dev@chrisarmitage.com>
2023-01-02 21:57:24 +0000

    Day 7, part 2

--- day07/day07.go
+++ day07/day07.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -73,6 +74,51 @@ func (e *Entry) CalculateTotalSize(upperLimit int) int {
 
 	return e.ActualSize
 
+}
+
+func (e *Entry) CalculateTotalSizeP2(lowerLimit int) int {
+	totalSize := 0
+	if len(e.Children) > 0 {
+		for _, child := range e.Children {
+			totalSize += child.CalculateTotalSizeP2(lowerLimit)
+		}
+
+		if totalSize >= lowerLimit {
+			fmt.Printf("Directory %s has a size of %d\n", e.Name, totalSize)
+			eligibleDirectories[totalSize] = e
+		}
+
+		return totalSize
+
+	}
+
+	return e.ActualSize
+
+} 
+
+
+var eligibleDirectories = make(map[int]*Entry)
+
+func (e *Entry) CalculateDirectoryToDeleteSize(totalDiscSize, requiredFreeSize int) int {
+	totalSizeInUse := e.getSize()
+	totalFreeSize := totalDiscSize - totalSizeInUse
+
+	fmt.Printf("Total disc size: %8d\n", totalDiscSize)
+	fmt.Printf("Total used size: %8d\n", totalSizeInUse)
+	fmt.Printf("Total free size: %8d\n", totalFreeSize)
+	fmt.Printf("Req free size  : %8d\n", requiredFreeSize)
+	fmt.Printf("To be freed    : %8d\n", requiredFreeSize - totalFreeSize)
+
+	_ = e.CalculateTotalSizeP2(requiredFreeSize - totalFreeSize)
+
+	// Sort the kets, get the first entry
+	keys := make([]int, 0)
+	for k, _ := range eligibleDirectories {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	return keys[0]
 } 
 
 func Run(sample bool) (string, error) {
@@ -89,6 +135,7 @@ func Run(sample bool) (string, error) {
 	root := parseInput(scanner)
 
 	root.CalculateTotalSize(100000)
+	resultPart2 := root.CalculateDirectoryToDeleteSize(70000000, 30000000)
 
 	//root := newEntry("/")
 
@@ -114,11 +161,13 @@ func Run(sample bool) (string, error) {
 	// fmt.Printf("Size C: %d\n", c.getSize())
 	// fmt.Printf("Size D: %d\n", d.getSize())
 
-	root.Print()
+	// root.Print()
 
 	return fmt.Sprintf(
-			"Part 1: Total filesize = %d\n", 
+		"Part 1: Total filesize = %d\n" +
+		"Part 2: Total filesize = %d\n", 
 			totalP1,
+			resultPart2,
 		),
 		 nil
 }
@@ -155,12 +204,12 @@ func parseInput(scanner *bufio.Scanner) *Entry {
 				if directoryName == ".." {
 					dirStack = dirStack[:len(dirStack)-1]
 					currentDirectory = dirStack[len(dirStack)-1:][0]
-					fmt.Printf("Debug: Moving up to %s\n", currentDirectory.Name)
+					// fmt.Printf("Debug: Moving up to %s\n", currentDirectory.Name)
 				} else if directoryName != "/" {
 					newDirectory := newEntry(directoryName)
-					fmt.Printf("Debug: Moving to %s\n", newDirectory.Name)
+					// fmt.Printf("Debug: Moving to %s\n", newDirectory.Name)
 					currentDirectory.AddChild(newDirectory)
-					fmt.Printf("Debug: Added %s to %s\n", newDirectory.Name, currentDirectory.Name)
+					// fmt.Printf("Debug: Added %s to %s\n", newDirectory.Name, currentDirectory.Name)
 
 					dirStack = append(dirStack, newDirectory)
 					currentDirectory = newDirectory
@@ -181,12 +230,12 @@ func parseInput(scanner *bufio.Scanner) *Entry {
 				line, "%d %s",
 				&fileSize,
 				&filename)
-			fmt.Printf("[line, size, name] [%s, %d, %s]\n", line, fileSize, filename)
+			// fmt.Printf("[line, size, name] [%s, %d, %s]\n", line, fileSize, filename)
 
 			if filename != "" {
 				newFile := newEntry(filename)
 				newFile.ActualSize = fileSize
-				fmt.Printf("Debug: Adding %s to %s\n", newFile.Name, currentDirectory.Name)
+				// fmt.Printf("Debug: Adding %s to %s\n", newFile.Name, currentDirectory.Name)
 				currentDirectory.AddChild(newFile)
 			}
 		}
